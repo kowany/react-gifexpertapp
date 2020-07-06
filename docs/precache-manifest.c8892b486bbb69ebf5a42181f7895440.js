aec7318a8ed960e42683f3cb2fa9e93ea57db63f
kowany <hvilla65@gmail.com>
2020-07-06 14:39:09 -0600

    docs agregado

---
+++ docs/precache-manifest.c8892b486bbb69ebf5a42181f7895440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7e2bf35650aa3628d9282c69f0da10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "deed8d7bf34edfa0b75d",
+    "url": "/static/css/main.1849e421.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deed8d7bf34edfa0b75d",
+    "url": "/static/js/main.f03adf12.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
